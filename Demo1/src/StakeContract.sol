0ca701fbf0866f4058f9c8cf4f837598045072ea
K1-R1 <kyranrawlinson@gmail.com>
2022-04-16 18:57:03 +0100

    Demo1: Created StakeContract and added remappings.txt

---
+++ Demo1/src/StakeContract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+error TransferFailed();
+
+contract StakeContract {
+    mapping(address => mapping(address => uint256)) public balances;
+
+    function stake(uint256 _amount, address _token) external returns (bool) {
+        balances[msg.sender][_token] += _amount;
+        bool success = IERC20(_token).transferFrom(
+            msg.sender,
+            address(this),
+            _amount
+        );
+        if (!success) revert TransferFailed();
+        return success;
+    }
+}
